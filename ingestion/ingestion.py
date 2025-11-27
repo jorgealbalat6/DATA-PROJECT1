18afd18772bc242ebdeae65fc0b9322210f22860
Javier Plaza Rosique <j.plazarosique@gmail.com>
2025-11-27 21:23:55 +0100

    cambios para configurar dbt

--- ingestion/ingestion.py
+++ ingestion/ingestion.py
@@ -23,7 +23,7 @@ def InsertarDatos(datos):
                         H11, V11, H12, V12, H13, V13, H14, V14, H15, V15, H16, V16, H17, V17, H18, V18, H19, V19,
                         H20, V20, H21, V21, H22, V22, H23, V23, H24, V24) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 
                 %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
-                 %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                 """, (datos['PUNTO_MUESTREO'], float(datos['MUNICIPIO']), float(datos['ESTACION']), float(datos['MAGNITUD']), float(datos['ANO']), float(datos['MES']), float(datos['DIA']),
                 float(datos['H01']), datos['V01'], float(datos['H02']), datos['V02'], float(datos['H03']), datos['V03'], float(datos['H04']), datos['V04'], float(datos['H05']), datos['V05'], float(datos['H06']), datos['V06'],
                 float(datos['H07']), datos['V07'], float(datos['H08']), datos['V08'], float(datos['H09']), datos['V09'], float(datos['H10']), datos['V10'], float(datos['H11']), datos['V11'], float(datos['H12']), datos['V12'],
