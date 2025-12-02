abcb1e8599eac78222a8c4f8555f8fb61971b16a
Javier Plaza Rosique <j.plazarosique@gmail.com>
2025-12-02 12:21:44 +0100

    inicio de los productores

--- productores/pob_general/alertas/alertas.py
+++ productores/pob_general/alertas/alertas.py
@@ -1,5 +1,22 @@
-# from confluent_kafka import Producer
-# import time
-# import json
+from confluent_kafka import Producer
+import time
+import json
+
+config = {
+    'bootstrap.servers': 'kafka:9092',
+    'client.id': 'alertas_pob_general'
+}
+
+productor = Producer(config)
 
 
+def enviar_alerta_pob_general(<aqui va en funcion del nombre de las columnas del dbt>):
+    texto = f"Alerta: {aqui va en funcion del nombre de las columnas del dbt}"
+    mensaje = {
+        "texto": texto
+    }
+    productor.produce(
+        topic = "alertas_pob_generales",
+        value = json.dumps(mensaje).encode("utf-8")
+    )
+    print("Alerta enviada.", texto)
--- productores/pob_general/datos/datos.py
+++ productores/pob_general/datos/datos.py
@@ -1,1 +1,22 @@
-print("hola")
+from confluent_kafka import Producer
+import time
+import json
+
+config = {
+    'bootstrap.servers': 'kafka:9092',
+    'client.id': 'datos_pob_general'
+}
+
+productor = Producer(config)
+
+
+def enviar_datos_pob_general(<aqui va en funcion del nombre de las columnas del dbt>):
+    texto = f"Datos: {aqui va en funcion del nombre de las columnas del dbt}"
+    mensaje = {
+        "texto": texto
+    }
+    productor.produce(
+        topic = "datos_pob_general",
+        value = json.dumps(mensaje).encode("utf-8")
+    )
+    print("Datos enviados.", texto)
