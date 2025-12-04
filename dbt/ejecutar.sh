b622b5c38aabfeb7acc4462cc874929edf11a9d9
Javier Plaza Rosique <j.plazarosique@gmail.com>
2025-12-04 08:19:27 +0100

    cambios DBT

--- dbt/ejecutar.sh
+++ dbt/ejecutar.sh
@@ -3,7 +3,9 @@
 # Bucle infinito
 while true; do
   echo "Iniciando dbt run..."
-  dbt run
+  cd proyecto_dbt
+  dbt run --profiles-dir .
+  cd ..
   echo "Esperando 1 hora (4000 segundos)..."
   sleep 4000 
 done
