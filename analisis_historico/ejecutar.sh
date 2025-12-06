78f9cdce57566c46c96edc177d86403ca39bbdaa
Javier Plaza Rosique <j.plazarosique@gmail.com>
2025-12-06 17:07:59 +0100

    cambios notebook

--- analisis_historico/ejecutar.sh
+++ analisis_historico/ejecutar.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "Iniciando ejecución del análisis histórico..."
 
-jupyter nbconvert --to html --execute --no-input --output-dir=/app analisis.ipynb
+jupyter nbconvert --to html --execute --output-dir=/app analisis.ipynb
 
 echo "Análisis histórico ejecutado exitosamente."
