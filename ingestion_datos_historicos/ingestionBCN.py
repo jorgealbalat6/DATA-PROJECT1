b5fbe17941ee4c9e9c546c493cb4b1e9f84a6ebb
Javier Plaza Rosique <j.plazarosique@gmail.com>
2025-12-07 01:16:58 +0100

    seguimiento ingestion datos historicos BCN

--- ingestion_datos_historicos/ingestionBCN.py
+++ ingestion_datos_historicos/ingestionBCN.py
@@ -15,7 +15,15 @@ for i in range(10):
         time.sleep(2)
 
 try:
-    df_historico_BCN = pd.read_csv("ingestion_datos_historicos/Qualitat_de_l’aire_als_punts_de_mesurament_manuals_de_la_Xarxa_de_Vigilància_i_Previsió_de_la_Contaminació_Atmosfèrica_20251206.csv", sep=";")
+    df_historico_BCN = pd.read_csv("Qualitat_de_l’aire_als_punts_de_mesurament_manuals_de_la_Xarxa_de_Vigilància_i_Previsió_de_la_Contaminació_Atmosfèrica_20251206.csv", sep=",")
     print("Se han leido correctamente el csv de Barcelona")
 except Exception as e:
     print("Error leyendo el csv de Barcelona:", e)
+
+try:
+    df_historico_BCN = df_historico_BCN[df_historico_BCN['NOM ESTACIÓ'].str.startswith('Barcelona', na=False)]
+    print("Se han eliminado correctamente los datos que no pertenecen a Barcelona")
+except Exception as e:
+    print("Error al eliminar los datos que no son de Barcelona:", e)
+
+
